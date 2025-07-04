0b934fa2a491aa4af65016bfbdf8049878143d4d
radthenone <krzyzak.codes@gmail.com>
2025-07-04 04:51:56 +0200

    change dashboard

--- frontend/src/app/shared/dashboard/dashboard.component.ts
+++ frontend/src/app/shared/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
